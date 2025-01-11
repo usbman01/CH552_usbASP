08fb06c3816d53d22ef1b5b2633d0e1a15c42355
usbman01 <thmona53@gmail.com>
2025-01-11 15:52:36 +0100

    Update build.bat

--- source/SDCC/build.bat
+++ source/SDCC/build.bat
@@ -1,9 +1,13 @@
+sdas8051 -losg .\obj\lowlevel ..\src\lowlevel.as
+sdas8051 -o .\obj\lowlevel.rel ..\src\lowlevel.as
+
 sdcc -c --opt-code-size --debug -o.\obj\ ..\src\clock.c
 sdcc -c --opt-code-size --debug -o.\obj\ ..\src\isp.c 
 sdcc -c --opt-code-size --debug -o.\obj\ ..\src\tpi.c
 sdcc -c --opt-code-size --debug -o.\obj\ ..\src\usbdrv.c
 sdcc -c --opt-code-size --debug -o.\obj\ ..\src\main.c
 
-sdcc --code-loc 0x0000 --no-xinit-opt .\obj\main.rel .\obj\clock.rel .\obj\isp.rel .\obj\tpi.rel .\obj\usbdrv.rel -o.\obj\usbasp.ihx 
+sdcc --code-loc 0x0000 --no-xinit-opt .\obj\main.rel .\obj\lowlevel.rel .\obj\clock.rel .\obj\isp.rel .\obj\tpi.rel .\obj\usbdrv.rel -o.\obj\usbasp.ihx 
+
 packihx .\obj\usbasp.ihx > .\obj\usbasp.hex
 pause
